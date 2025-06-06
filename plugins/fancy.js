3ddf8820657e79eb3d4ab34f4a2891064edbdcc9
Dilshan099987 <ashinsadilshan494@gmail.com>
2025-06-06 16:46:58 +0530

    Update fancy.js

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -22,7 +22,7 @@ cmd({
     }
 
     const fonts = response.data.result.map(item => `*${item.name}:*\n${item.result}`).join("\n\n");
-    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *Powered by AWAIS MD*`;
+    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *Powered by 𝐃𝐈𝐋𝐒𝐇𝐀𝐍 𝐌𝐃*`;
 
     await conn.sendMessage(from, { text: resultText }, { quoted: m });
   } catch (error) {
