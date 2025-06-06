31f0790ee760d7e134c43993d4406a96e841cd05
Dilshan099987 <ashinsadilshan494@gmail.com>
2025-06-06 16:38:47 +0530

    Update ping2.js

--- plugins/ping2.js
+++ plugins/ping2.js
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `*DILSHAN-MD SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `*🚀DILSHAN-MD SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
         await conn.sendMessage(from, {
             text,
@@ -41,8 +41,8 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: 'https://whatsapp.com/channel/0029Vb691MgG8l5GHg8HgI3b',
-                    newsletterName: "DILSHAN ᴍᴅ",
+                    newsletterJid: '120363419308807922@newsletter',
+                    newsletterName: "𝐃𝐈𝐋𝐒𝐇𝐀𝐍_𝐌𝐃",
                     serverMessageId: 143
                 }
             }
